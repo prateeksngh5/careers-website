2045811d34fcf9d4589e5739e7a222e050a82a2c
Prateek Singh <63698993+prateeksngh5@users.noreply.github.com>
2022-12-08 10:00:21 +0000

    Added HTML & CSS

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-#from the module flask importing the class Flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "hello, prateek!!"
+  return render_template("home.html")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
